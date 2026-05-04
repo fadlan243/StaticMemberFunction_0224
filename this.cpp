b06916289360217e65b090d0ba25bad54483af8b
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 19:58:52 +0700

    add library

--- this.cpp
+++ this.cpp
@@ -4,5 +4,15 @@ using namespace std;
 class buku{
     string judul;
     public:
-
+        string setget(string judul){
+            this->judul= judul; //nilai parameter 'judul' untuk member variabel 'judul'
+            return this->judul; // return variabel judul.
+        }
 };
+
+int main()
+{
+    buku bukunya;
+    cout<<bukunya.setget ("Matematika");
+    return 0;
+}
