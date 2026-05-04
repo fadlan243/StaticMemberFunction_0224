02856d1736526ebef0692ca3069d765b17ae0028
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 20:02:39 +0700

    implementasi method cetak

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -32,3 +32,9 @@ mahasiswa :: mahasiswa(int iNim, string iNama) {
     nim = iNim; // definisi NIM dan NAMA
     nama = iNama;
 }
+
+// Implementasi Method Cetak
+void mahasiswa::cetak() {
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
