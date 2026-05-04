74080ae9659244ed518708e2022da9999ff3c674
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 20:03:30 +0700

    menambahkan kelas mahasiswa

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -3,18 +3,5 @@
 using namespace std;
 
 class mahasiswa {
-public:
-    mahasiswa(int nim, string nama); //constructor dengan parameter
-};
 
-//Definisi constructor dengan parameter
-mahasiswa::mahasiswa(int nim, string nama) {
-    cout << "Constructor Dengan Parameter Terpanggil" << endl;
-    cout << "NIM : " << nim << endl;
-    cout << "Nama : " << nama << endl;
-}
-
-int main(){
-    mahasiswa mhs(12345, "Pascal"); //memanggil constructor dengan parameter
-    return 0;
 }
