7c24e607ae065782f3f1bc0a763e1fa90fcf8527
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 20:07:13 +0700

    pembuatan static function

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -9,6 +9,21 @@ private:
 public:
         int id;
         string nama;
+        
+        void setID();
+        void printAll();
 
+        //pembuatan static function
+        static void setNim(int pnim) {
+            nim = pnim;
+        }
+
+        static int getNim() {
+            return nim;
+        }
+
+        Mahasiswa(string pnama) :nama(pnama) {
+            setID();
+        };
 };
 
