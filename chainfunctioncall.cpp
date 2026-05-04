00aed48782814626a988dd33fcf86254f1eb6d44
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 19:02:23 +0700

    first commit

---
+++ chainfunctioncall.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class buku{
+    string judul;
+
+    public:
+    buku &setJudul(string judul){
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul(){
+        return this->judul;
+    }
+};
+
+int main(){
+    buku bukunya;
+    //bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
+};
