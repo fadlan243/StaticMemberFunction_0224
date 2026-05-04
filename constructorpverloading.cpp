39e541cac8f13ac5634f9ad45f5b6423b0310584
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 20:02:18 +0700

    definisi hanya nama

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -23,3 +23,8 @@ mahasiswa :: mahasiswa() {
 mahasiswa :: mahasiswa(int iNim) {
     nim = iNim; // definisi hanya NIM
 }
+
+mahasiswa :: mahasiswa(string iNama) {
+    nama = iNama; // definisi hanya NAMA
+}
+
