934734d4661a946ef96bb20ef1c9ac194ddda2d6
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 19:59:13 +0700

    menambahkan kelas mahasiswa

--- constructor.cpp
+++ constructor.cpp
@@ -2,15 +2,6 @@
 using namespace std;
 
 class mahasiswa {
-public:
-    mahasiswa(); // Constructor
+
 };
 
-mahasiswa::mahasiswa() {
-    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
-}
-
-int main() {
-    mahasiswa mhs; // Pembuatan Object
-    return 0;
-}
