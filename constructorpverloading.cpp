3919c55a28e0615944d0727a736363443bcfb4b9
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-05-04 20:01:46 +0700

    implementasi constructor

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -8,5 +8,15 @@ private:
     string nama;
 
 public:
-
+    mahasiswa();
+    mahasiswa(int iNim);
+    mahasiswa(string iNama);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
+
+// Implementasi Constructor
+mahasiswa :: mahasiswa() {
+    // Constructor kosong
+}
+
